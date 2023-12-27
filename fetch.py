682b7b80c921acc3d6f2ee0cc371b09bcb9f96da
Lefia <lefia1222@gmail.com>
2023-12-27 16:55:20 +0800

    Add request example

--- fetch.py
+++ fetch.py
@@ -1,4 +1,18 @@
+import pandas as pd
+import requests as req
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
 def fetch_weather():
 	url = 'https://opendata.cwa.gov.tw/api/v1/rest/datastore/O-A0002-001'
-
+	headers = {'Authorization': os.getenv('API_KEY')}
+	res = req.get(url, headers=headers)
+	if res.status_code == 200:
+		json_data = res.json()
+		df = pd.json_normalize(json_data, ['records', 'Station'])
+		print(df['StationName'].value_counts())
+	else:
+		print('Error when requesting data')
 	return 
