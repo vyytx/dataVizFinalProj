b6c8d2b1b795b8f52c957f4c5ecdb34c14fb06b4
Lefia <lefia1222@gmail.com>
2024-01-02 13:26:01 +0800

    Add search function to dropdown

--- static/js/main.js
+++ static/js/main.js
@@ -3,16 +3,33 @@ const toggleMenu = () => {
     menu.classList.toggle('hidden')
 }
 
-const dropdownList = document.getElementById('dropdownList')
+const dropdownMenu = document.getElementById('dropdownMenu')
 const dropdownButton = document.getElementById('dropdownButton')
+const dropdownSearch = document.getElementById('dropdownSearch')
+const dropdownList = document.getElementById('dropdownList')
 
 dropdownButton.addEventListener('click', (event) => {
     event.stopPropagation()
-    dropdownList.classList.toggle('hidden')
+    dropdownMenu.classList.toggle('hidden')
 })
 
-document.documentElement.addEventListener('click', () => {
-    if (!dropdownList.classList.contains('hidden')) {
-        dropdownList.classList.toggle('hidden')
+document.documentElement.addEventListener('click', (event) => {
+    if (!dropdownMenu.classList.contains('hidden') && !dropdownMenu.contains(event.target)) {
+        dropdownMenu.classList.toggle('hidden')
     }
+})
+
+dropdownSearch.addEventListener('input', () => {
+    const inputText = dropdownSearch.value
+    const searchText = inputText.replace('台', '臺')
+
+    const listItems = dropdownList.getElementsByTagName('li')
+    for (const listItem of listItems) {
+        const anchor = listItem.querySelector('a')
+        if (!anchor.innerText.includes(searchText)) {
+          listItem.classList.add('hidden')
+        } else {
+          listItem.classList.remove('hidden')
+        }
+      }
 })
