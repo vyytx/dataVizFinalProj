12f8aa0705afbb6044d1b29b79f96b18a9eb1314
Lefia <lefia1222@gmail.com>
2024-01-02 15:56:01 +0800

    add some style and fix errors

--- static/js/chart.js
+++ static/js/chart.js
@@ -4,54 +4,85 @@
  * @param {string} element 
  */
 const choropleth = (chart_data, element) => {
-    const data = [{
-      type: 'choroplethmapbox', 
-      name: 'Taiwan', 
-      geojson: 'static/counties.json', 
-      locations: unpack(chart_data, 'location'),
-      z: unpack(chart_data, 'z')
-    }]
-    const layout = {
-      mapbox: {
-        style: 'white-bg', 
-        center: {lon: 120.9738819, lat: 23.97565}, 
-        zoom: 5.7
-      },
-      showlegend: false,
-      margin: {t: 30, b: 10, l: 10, r:10},
-    };
-    const config = {
-      responsive: true
-    }
-    Plotly.newPlot(element, data, layout, config)
-      .then((chart) => {
-        chart.on('plotly_click', (data) => {
-          const countyName = data.points[0].location
-          window.location.assign(`/county/${countyName}`)
-        })
+  const data = [{
+    type: 'choroplethmapbox',
+    name: 'Taiwan',
+    geojson: 'static/counties.json',
+    locations: unpack(chart_data, 'location'),
+    z: unpack(chart_data, 'z'),
+  }]
+  const layout = {
+    width: 500,
+    mapbox: {
+      style: 'white-bg',
+      center: { lon: 120.9738819, lat: 23.97565 },
+      zoom: 5.7
+    },
+    showlegend: false,
+    margin: { t: 30, b: 10, l: 10, r: 10 },
+  };
+  const config = {
+    responsive: true
+  }
+  Plotly.newPlot(element, data, layout, config)
+    .then((chart) => {
+      chart.on('plotly_click', (data) => {
+        const countyName = data.points[0].location
+        window.location.assign(`/county/${countyName}`)
       })
-    
+    })
+
+}
+
+const temperature_forecast_scatter = (chart_data, element) => {
+  const data = [
+    {
+      type: 'scatter',
+      mode: 'lines+markers',
+      name: '最低溫度',
+      x: chart_data['x'],
+      y: chart_data['MinT'],
+      hovertemplate: '%{y}°C'
+    },
+    {
+      type: 'scatter',
+      mode: 'lines+markers',
+      name: '最高溫度',
+      x: chart_data['x'],
+      y: chart_data['MaxT'],
+      hovertemplate: '%{y}°C'
+    },
+  ]
+
+  const layout = {
+    title: '未來 36 小時最高和最低溫度',
+    xaxis: {
+      tickmode: 'array',
+      tickvals: chart_data['ticks']
+    },
+    margin: { t: 100 },
+  };
+  const config = {
+    responsive: true
+  }
+  Plotly.newPlot(element, data, layout, config)
+
 }
 
 // 創建兩種不同排序的直方圖
-const createHistogram = (chart_data, element) => {
+const createHistogram = (chart_data, element, yaxix_title) => {
   // 將資料轉換為直方圖需要的格式
-  const sortedData = chart_data.slice().sort((a, b) => a.z - b.z); // 按照氣溫排序
-
-  const locations = chart_data.map(item => item.location);
-  const temperatures = chart_data.map(item => item.z);
-
-  const sortedLocations = sortedData.map(item => item.location);
-  const sortedTemperatures = sortedData.map(item => item.z);
+  const sorted_chart_data = _.cloneDeep(chart_data); 
+  sorted_chart_data.sort((a, b) => b.z - a.z)// 按照氣溫排序
 
   // 創建直方圖的 trace，並使用色彩映射
   const trace1 = {
-    x: locations, // 將縣市放在 x 軸
-    y: temperatures, // 將氣溫放在 y 軸
+    x: unpack(chart_data, 'location'), // 將縣市放在 x 軸
+    y: unpack(chart_data, 'z'), // 將氣溫放在 y 軸
     type: 'bar',
     orientation: 'v', // 垂直方向顯示直方圖
     marker: {
-      color: temperatures, // 根據溫度資料設定顏色
+      color: unpack(chart_data, 'z'), // 根據溫度資料設定顏色
       colorscale: 'Reds', // 設定紅色系漸層
     },
     name: '未排序',
@@ -59,12 +90,12 @@ const createHistogram = (chart_data, element) => {
   };
 
   const trace2 = {
-    x: sortedLocations, // 將縣市放在 x 軸
-    y: sortedTemperatures, // 將氣溫放在 y 軸
+    x: unpack(sorted_chart_data, 'location'), // 將縣市放在 x 軸
+    y: unpack(sorted_chart_data, 'z'), // 將氣溫放在 y 軸
     type: 'bar',
     orientation: 'v', // 垂直方向顯示直方圖
     marker: {
-      color: sortedTemperatures, // 根據溫度資料設定顏色
+      color: unpack(sorted_chart_data, 'z'), // 根據溫度資料設定顏色
       colorscale: 'Reds', // 設定紅色系漸層
     },
     name: '排序',
@@ -75,13 +106,13 @@ const createHistogram = (chart_data, element) => {
 
   // 創建直方圖的 layout
   const layout = {
-    title: '各縣市比較',
+    width: 600,
     xaxis: {
       title: '縣市', // 修改 x 軸標題
       tickangle: -45,
     },
     yaxis: {
-      title: '平均數值', // 修改 y 軸標題
+      title: yaxix_title, // 修改 y 軸標題
     },
     margin: {
       l: 100,
